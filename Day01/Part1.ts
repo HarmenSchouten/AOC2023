4bdf63b6448c5cb4e81a1f0d853a97d70667162a
Harmen <harmenschouten@outlook.com>
2023-12-01 07:08:05 +0100

    Day 1

--- Day01/Part1.ts
+++ Day01/Part1.ts
@@ -1,1 +1,10 @@
 const text = await Deno.readTextFile("./Day01/input.txt")
+
+const answer = text
+    .split("\r\n")
+    .map(item => [...item].filter(char => !isNaN(Number(char))))
+    .filter(numbers => numbers.length >= 1)
+    .map(numbers => Number(`${numbers[0]}${numbers[numbers.length - 1]}`))
+    .reduce((acc, state) => acc += state, 0)
+
+console.log(answer)
--- Day01/Part2.ts
+++ Day01/Part2.ts
@@ -1,1 +1,31 @@
 const text = await Deno.readTextFile("./Day01/input.txt")
+const numbers:Record<string, number> = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+};
+
+const answer = text
+    .split(/\r\n/g)
+    .reduce((sum, line) => {
+        const nums = [];
+        for (let i = 0; i < line.length; i++) {
+            Object.keys(numbers).forEach(num => {
+                if (line.slice(i).startsWith(num)) {
+                    nums.push(numbers[num]);
+                }
+            });
+            if (!isNaN(Number(line[i]))) {
+                nums.push(parseInt(line[i]));
+            }
+        }
+        return sum + nums[0] * 10 + nums[nums.length - 1];
+    }, 0);
+
+console.log(answer);
