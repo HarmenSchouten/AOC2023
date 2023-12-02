2825b870520067e111ac6a1b102f23c670987fea
Harmen <harmenschouten@outlook.com>
2023-12-02 15:03:24 +0100

    product fn on number array

--- utils/arrayUtils.ts
+++ utils/arrayUtils.ts
@@ -2,12 +2,21 @@ declare global {
     interface Array<T> {
         /** Gets the sum of all integers in an array */
         sum(): number
+        /** Gets the product of all integers in an array */
+        product(): number
     }
 }
 
 Array.prototype.sum = function () {
     if (this.every((value) => typeof value === 'number')) {
         return this.reduce((acc, state) => acc += state, 0)
+    }
+    throw new Error("This array does not contain only numbers")
+}
+
+Array.prototype.product = function () {
+    if (this.every((value) => typeof value === 'number')) {
+        return this.reduce((acc, state) => acc *= state, 1)
     }
     throw new Error("This array does not contain only numbers")
 }
