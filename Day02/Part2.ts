a304e5a8d381bbe949f185421f09c48fbde9569a
Harmen <harmenschouten@outlook.com>
2023-12-02 06:38:29 +0100

    Day 2

--- Day02/Part2.ts
+++ Day02/Part2.ts
@@ -15,26 +15,22 @@ const answer = text
         .split("; ")
         .flatMap(item => 
             item.split(", ").map(item => item.split(" ")).map(([value, key]) => ({key: key, value: Number(value)}))))
-    .map((item, index) => ({
-        id: index + 1,
-        limits: item.reduce((acc, {key, value}) => {
-            if (!(key in limitMap)) return acc
-            if (acc[key] === undefined) {
-                acc[key] = value
-                return acc
-            }
-            if (acc[key] <= value) {
-                acc[key] = value
-            }
+    .map(item => item.reduce((acc, {key, value}) => {
+        if (!(key in limitMap)) return acc
+        if (acc[key] === undefined) {
+            acc[key] = value
             return acc
-        }, {} as Record<string, number>)
-    }))
-    .map(item => ({
-        id: item.id,
-        power: Object.keys(item.limits).reduce((acc, state) => acc === 0
-            ? item.limits[state]
-            : acc * item.limits[state], 0)   
-    }))
-    .reduce((acc, state) => acc += state.power, 0)
+        }
+        if (acc[key] <= value) {
+            acc[key] = value
+        }
+        return acc
+    }, {} as Record<string, number>))
+    .map(item => Object
+        .keys(item)
+        .reduce((acc, state) => acc === 0
+            ? item[state]
+            : acc * item[state], 0))
+    .reduce((acc, state) => acc += state, 0)
 
 console.log(answer)
