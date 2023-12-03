b13f1d9a93bc6d44b2da455f4404b8cc1e5a6c86
Harmen <harmenschouten@outlook.com>
2023-12-03 07:18:02 +0100

    Day03

--- Day03/Part1.ts
+++ Day03/Part1.ts
@@ -1,1 +1,42 @@
+import "../utils/index.ts"
+
 const text = await Deno.readTextFile("./Day03/input.txt")
+
+const grid = text
+    .splitLb()
+    .flatMap((line, y) => line.split("").map((item, x) => ({x: x, y: y, value: item})))
+
+const identifiers = grid.filter(item => item.value !== "." && item.value.ints().length === 0)
+
+const manhattan = (x0: number, x1: number, y0: number, y1: number) => Math.abs(x1-x0) + Math.abs(y1-y0);
+
+const answer = identifiers
+    .map(item => [
+        grid.filter(gridItem => gridItem.x === item.x && gridItem.y === item.y - 1)?.[0],
+        grid.filter(gridItem => gridItem.x === item.x && gridItem.y === item.y + 1)?.[0],
+        grid.filter(gridItem => gridItem.x === item.x - 1 && gridItem.y === item.y)?.[0],
+        grid.filter(gridItem => gridItem.x === item.x + 1 && gridItem.y === item.y)?.[0],
+        grid.filter(gridItem => gridItem.x === item.x - 1 && gridItem.y === item.y - 1)?.[0],
+        grid.filter(gridItem => gridItem.x === item.x + 1 && gridItem.y === item.y - 1)?.[0],
+        grid.filter(gridItem => gridItem.x === item.x - 1 && gridItem.y === item.y + 1)?.[0],
+        grid.filter(gridItem => gridItem.x === item.x + 1 && gridItem.y === item.y + 1)?.[0],
+    ])
+    .flatMap(item => item.filter(item => item !== undefined && item.value.ints().length > 0))
+    .filter((item, idx, array) => array.findIndex(aItem => aItem.x === item.x && aItem.y === item.y) === idx)
+    .reduce((acc ,state) => {
+        const has0Distance = acc.find(item => manhattan(item.x, state.x, item.y, state.y) <= 1)
+        if (has0Distance) return acc
+        return [...acc, state]
+    }, [] as {x: number, y: number, value: string}[])
+    .map(item => {
+        const line = grid.filter(gridItem => gridItem.y === item.y).map(item => item.value).join("")
+        const before = [...line.slice(0, item.x)].findLastIndex(item => !item.isDigit())
+        const beforeIdx = before === -1 ? 0 : before + 1
+        const after = [...line.slice(item.x)].findIndex(item => !item.isDigit())
+        const afterIdx = after === -1 ? line.length : item.x + after
+
+        return Number(line.slice(beforeIdx, afterIdx))
+    })
+    .sum()
+
+console.log(answer)
--- Day03/Part2.ts
+++ Day03/Part2.ts
@@ -1,1 +1,45 @@
+import "../utils/index.ts"
+
 const text = await Deno.readTextFile("./Day03/input.txt")
+
+const grid = text
+    .splitLb()
+    .flatMap((line, y) => line.split("").map((item, x) => ({x: x, y: y, value: item})))
+
+const identifiers = grid.filter(item => item.value === "*" && item.value.ints().length === 0)
+
+const manhattan = (x0: number, x1: number, y0: number, y1: number) => Math.abs(x1-x0) + Math.abs(y1-y0);
+
+const answer = identifiers
+    .map(item => [
+        grid.find(gridItem => gridItem.x === item.x && gridItem.y === item.y - 1),
+        grid.find(gridItem => gridItem.x === item.x && gridItem.y === item.y + 1),
+        grid.find(gridItem => gridItem.x === item.x - 1 && gridItem.y === item.y),
+        grid.find(gridItem => gridItem.x === item.x + 1 && gridItem.y === item.y),
+        grid.find(gridItem => gridItem.x === item.x - 1 && gridItem.y === item.y - 1),
+        grid.find(gridItem => gridItem.x === item.x + 1 && gridItem.y === item.y - 1),
+        grid.find(gridItem => gridItem.x === item.x - 1 && gridItem.y === item.y + 1),
+        grid.find(gridItem => gridItem.x === item.x + 1 && gridItem.y === item.y + 1),
+    ])
+    .map(item => item.filter(item => item !== undefined && item.value.ints().length > 0))
+    .map(item => {
+        const subItems = item
+            .reduce((acc ,state) => {
+                const has0Distance = acc.find(item => manhattan(item.x, state!.x, item.y, state!.y) <= 1)
+                if (has0Distance || !state) return acc
+                return [...acc, state]
+            }, [] as {x: number, y: number, value: string}[])
+        if (subItems.length != 2) return 0
+        return subItems
+            .map((item) => {
+                const line = grid.filter(gridItem => gridItem.y === item.y).map(item => item.value).join("")
+                const before = [...line.slice(0, item.x)].findLastIndex(item => !item.isDigit())
+                const beforeIdx = before === -1 ? 0 : before + 1
+                const after = [...line.slice(item.x)].findIndex(item => !item.isDigit())
+                const afterIdx = after === -1 ? line.length : item.x + after
+
+                return Number(line.slice(beforeIdx, afterIdx))
+            }).product()
+    }).sum()
+
+console.log(answer)
