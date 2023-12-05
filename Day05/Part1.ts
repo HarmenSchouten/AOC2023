8fc3836d9050c0d331eff529eb0163bafc403ea8
Harmen <harmenschouten@outlook.com>
2023-12-05 07:31:20 +0100

    Day05

--- Day05/Part1.ts
+++ Day05/Part1.ts
@@ -1,1 +1,52 @@
+import "../utils/index.ts"
+
 const text = await Deno.readTextFile("./Day05/input.txt")
+
+const seeds = [] as number[]
+
+const parts = text
+    .splitDlb()
+    .map((item, idx) => {
+        const rows = item.splitLb();
+        if (idx === 0) {
+            const [_, values] = rows[0].split(":")
+            values.split(" ").map(item => Number(item)).forEach(item => {
+                seeds.push(item)
+            })
+            return undefined
+        } else {
+            return {
+                identifier: rows[0].replace(":", "").replace(" map", ""),
+                map: rows.slice(1).map(row => {
+                    const items = row.split(" ").map(Number)
+                    return {
+                        destination: items[0],
+                        source: items[1],
+                        range: items[2]
+                    }
+                })
+            }
+        }
+    })
+    .filter(item => !!item)
+
+const answer = parts
+    .reduce((acc, part) => {
+        if (!part) return acc
+        const newSeeds = [] as number[]
+
+        acc.forEach(seed => {
+            const sourceMap = part?.map.find(map => map.source <= seed && (map.source + map.range) >= seed)
+            if (!sourceMap) {
+                newSeeds.push(seed)
+            } else {
+                const diff = sourceMap.destination - sourceMap.source
+                newSeeds.push(seed + diff)
+            }
+        })
+        return newSeeds
+    }, seeds as number[])
+    .sort((a, b) => a - b)
+    .shift()
+
+console.log(answer)
--- Day05/Part2.ts
+++ Day05/Part2.ts
@@ -1,1 +1,63 @@
+import "../utils/index.ts"
+
 const text = await Deno.readTextFile("./Day05/input.txt")
+type Seed = {
+    start: number
+    end: number
+}
+const seeds = [] as Seed[]
+
+const parts = text
+    .splitDlb()
+    .map((item, idx) => {
+        const rows = item.splitLb();
+        if (idx === 0) {
+            const [_, values] = rows[0].split(": ")
+            const numbers = values.split(" ").map(item => Number(item))
+            for (let i = 0; i < numbers.length; i+=2) {
+                const curr  = numbers[i]
+                const next = numbers[i + 1]
+                seeds.push({
+                    start: curr,
+                    end: curr + next
+                })
+            }
+            return undefined
+        } else {
+            return {
+                identifier: rows[0].replace(":", "").replace(" map", ""),
+                map: rows.slice(1).map(row => {
+                    const items = row.split(" ").map(Number)
+                    return {
+                        destination: items[0],
+                        source: items[1],
+                        range: items[2]
+                    }
+                })
+            }
+        }
+    })
+    .filter(item => !!item)
+
+const answer = parts
+    .reduce((acc, part) => {
+        if (!part) return acc;
+        const newSeeds = [] as Seed[]
+        acc.forEach(seed => {
+            part.map.forEach(map => {
+                const max = Math.max(map.source, seed.start)
+                const min = Math.min(map.source + map.range, seed.end)
+                if (max < min) {
+                    newSeeds.push({
+                        start: max + map.destination - map.source,
+                        end: min + map.destination - map.source
+                    })
+                }
+            })
+        })
+        return newSeeds
+    }, seeds)
+    .sort((a, b) => a.start - b.start)
+    .shift()
+
+console.log(answer?.start)
