4a3487903909c74171b459810a036dea89f3e36a
Harmen <harmenschouten@outlook.com>
2023-12-06 06:40:34 +0100

    Day 6

--- Day06/Part1.ts
+++ Day06/Part1.ts
@@ -1,1 +1,32 @@
+import "../utils/index.ts"
+
 const text = await Deno.readTextFile("./Day06/input.txt")
+
+const lines = text
+    .splitLb()
+    .map(line => line.split(": ")[1].split(" ").filter(item => item !== "").map(Number))
+
+type Race = {
+    time: number
+    distance: number
+}
+const races = [] as Race[]
+for (let i = 0; i < lines[0].length; i++) {
+    races.push({
+        time: lines[0][i],
+        distance: lines[1][i]
+    })
+}
+
+let answer = 1;
+races.forEach(race => {
+    const speeds = new Set<number>()
+    for (let i = 0; i < race.time; i++) {
+        if (((race.time - i) * i) > race.distance) {
+            speeds.add(i)
+        }
+    }
+    answer *= speeds.size
+})
+
+console.log(answer)
--- Day06/Part2.ts
+++ Day06/Part2.ts
@@ -1,1 +1,20 @@
+import "../utils/index.ts"
+
 const text = await Deno.readTextFile("./Day06/input.txt")
+
+const lines = text
+    .splitLb()
+    .map(line => Number(line.split(": ")[1].split("").filter(item => item.isDigit()).join("")))
+
+const races = [{time: lines[0], distance: lines[1]}]
+
+let answer = 0;
+races.forEach(race => {
+    for (let i = 0; i < race.time; i++) {
+        if (((race.time - i) * i) > race.distance) {
+            answer += 1
+        }
+    }
+})
+
+console.log(answer)
