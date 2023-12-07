dd9cbb09ed100bf35ac4672061f1248e65a909c4
Harmen <harmenschouten@outlook.com>
2023-12-07 17:21:52 +0100

    Init

---
+++ Day09/Part2.ts
@@ -0,0 +1,3 @@
+import "../utils/index.ts"
+
+const text = await Deno.readTextFile("./Day09/input.txt")
---
+++ Day13/Part2.ts
@@ -0,0 +1,3 @@
+import "../utils/index.ts"
+
+const text = await Deno.readTextFile("./Day13/input.txt")
