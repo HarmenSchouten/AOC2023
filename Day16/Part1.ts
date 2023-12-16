77917fc22e5968f735b9c18ab17ad68e80acdb85
Harmen <harmenschouten@outlook.com>
2023-12-16 10:56:16 +0100

    Optimizations

--- Day16/Part1.ts
+++ Day16/Part1.ts
@@ -11,21 +11,22 @@ const text = await Deno.readTextFile("./Day16/input.txt")
 
 const grid = new Grid<string>(text)
 
+const start = grid.cells.find(item => item.x === 0 && item.y === 0)!
+
 const queue = [{
-    cell: grid.cells.find(item => item.x === 0 && item.y === 0)!,
-    direction: "D"
+    cell: start,
+    direction: (start.value === "\\" || start.value === "|") ? "D" : "R"
 }] as QueueItem[]
+
 const visited = new Set<string>()
 
 const cellToKey = (cell: GridCell<string>, direction: string) => `${cell.x},${cell.y}.${direction}`
 
 const getNextCell = (item: QueueItem) => {
-    switch (item.direction) {
-        case "U": return grid.cells.find(cell => cell.x === item.cell.x && cell.y === item.cell.y - 1)
-        case "D": return grid.cells.find(cell => cell.x === item.cell.x && cell.y === item.cell.y + 1)
-        case "L": return grid.cells.find(cell => cell.x === item.cell.x - 1 && cell.y === item.cell.y)
-        case "R": return grid.cells.find(cell => cell.x === item.cell.x + 1 && cell.y === item.cell.y)
-    }
+    if (item.direction === "U") return item.cell.up()
+    if (item.direction === "D") return item.cell.down()
+    if (item.direction === "L") return item.cell.left()
+    if (item.direction === "R") return item.cell.right()
 }
 
 while (queue.length > 0) {
@@ -33,26 +34,24 @@ while (queue.length > 0) {
 
     const nextCell = getNextCell(cell)
 
-    if (visited.has(cellToKey(cell.cell, cell.direction))) continue;
+    const key = cellToKey(cell.cell, cell.direction)
 
-    if (!nextCell) {
-        visited.add(cellToKey(cell.cell, cell.direction))
-        continue;
-    }
+    if (visited.has(key)) continue;
+
+    visited.add(key)
+
+    if (!nextCell) continue;
 
     switch (nextCell.value) {
         case "/": {
-            visited.add(cellToKey(cell.cell, cell.direction))
             queue.push({ cell: nextCell, direction: cell.direction === "U" ? "R" : cell.direction === "R" ? "U" : cell.direction === "D" ? "L" : "D"})
             break;
         }
         case "\\": {
-            visited.add(cellToKey(cell.cell, cell.direction))
             queue.push({ cell: nextCell, direction: cell.direction === "U" ? "L" : cell.direction === "L" ? "U" : cell.direction === "D" ? "R" : "D"})
             break;
         }
         case "-": {
-            visited.add(cellToKey(cell.cell, cell.direction))
             if (cell.direction === "U" || cell.direction === "D") {
                 queue.push({ cell: nextCell, direction: "R" })
                 queue.push({ cell: nextCell, direction: "L" })
@@ -62,7 +61,6 @@ while (queue.length > 0) {
             break;
         }
         case "|": {
-            visited.add(cellToKey(cell.cell, cell.direction))
             if (cell.direction === "L" || cell.direction === "R") {
                 queue.push({ cell: nextCell, direction: "U" })
                 queue.push({ cell: nextCell, direction: "D" })
@@ -72,29 +70,18 @@ while (queue.length > 0) {
             break;
         }
         case ".": {
-            visited.add(cellToKey(cell.cell, cell.direction))
             queue.push({ cell: nextCell, direction: cell.direction })
             break;
         }
     }
 }
 
-const values = [...visited.values()].map(item => {
-    const [x, y] = item.split(".")[0].split(",")
-    return { x: parseInt(x), y: parseInt(y) }
-})
-.filter((item, idx, arr) => arr.findIndex(i => i.x === item.x && i.y === item.y) === idx)
-// console.log(visited, values)
+const answer = [...visited.values()]
+    .map(item => {
+        const [x, y] = item.split(".")[0].split(",").map(Number)
+        return { x, y }
+    })
+    .filter((item, idx, arr) => arr.findIndex(i => i.x === item.x && i.y === item.y) === idx)
+    .length
 
-for (let y = 0; y < grid.height; y++) {
-    let line = ""
-    for (let x = 0; x < grid.width; x++) {
-        if (values.find(item => item.x === x && item.y === y)) {
-            line += "#"
-        } else {
-            line += "."
-        }
-    }
-    console.log(line)
-}
-console.log(values.length)
+console.log(answer)
--- Day16/Part2.ts
+++ Day16/Part2.ts
@@ -14,12 +14,10 @@ const grid = new Grid<string>(text)
 const cellToKey = (cell: GridCell<string>, direction: string) => `${cell.x},${cell.y}.${direction}`
 
 const getNextCell = (item: QueueItem) => {
-    switch (item.direction) {
-        case "U": return grid.cells.find(cell => cell.x === item.cell.x && cell.y === item.cell.y - 1)
-        case "D": return grid.cells.find(cell => cell.x === item.cell.x && cell.y === item.cell.y + 1)
-        case "L": return grid.cells.find(cell => cell.x === item.cell.x - 1 && cell.y === item.cell.y)
-        case "R": return grid.cells.find(cell => cell.x === item.cell.x + 1 && cell.y === item.cell.y)
-    }
+    if (item.direction === "U") return item.cell.up()
+    if (item.direction === "D") return item.cell.down()
+    if (item.direction === "L") return item.cell.left()
+    if (item.direction === "R") return item.cell.right()
 }
 
 const doRunForStartCoord = (x: number, y: number, direction: string) => {
@@ -27,33 +25,32 @@ const doRunForStartCoord = (x: number, y: number, direction: string) => {
         cell: grid.cells.find(item => item.x === x && item.y === y)!,
         direction: direction
     }] as QueueItem[]
+    
     const visited = new Set<string>()
 
     while (queue.length > 0) {
         const cell = queue.shift()!
     
         const nextCell = getNextCell(cell)
+
+        const key = cellToKey(cell.cell, cell.direction)
     
-        if (visited.has(cellToKey(cell.cell, cell.direction))) continue;
+        if (visited.has(key)) continue;
     
-        if (!nextCell) {
-            visited.add(cellToKey(cell.cell, cell.direction))
-            continue;
-        }
+        visited.add(key)
+
+        if (!nextCell) continue;
     
         switch (nextCell.value) {
             case "/": {
-                visited.add(cellToKey(cell.cell, cell.direction))
                 queue.push({ cell: nextCell, direction: cell.direction === "U" ? "R" : cell.direction === "R" ? "U" : cell.direction === "D" ? "L" : "D"})
                 break;
             }
             case "\\": {
-                visited.add(cellToKey(cell.cell, cell.direction))
                 queue.push({ cell: nextCell, direction: cell.direction === "U" ? "L" : cell.direction === "L" ? "U" : cell.direction === "D" ? "R" : "D"})
                 break;
             }
             case "-": {
-                visited.add(cellToKey(cell.cell, cell.direction))
                 if (cell.direction === "U" || cell.direction === "D") {
                     queue.push({ cell: nextCell, direction: "R" })
                     queue.push({ cell: nextCell, direction: "L" })
@@ -63,7 +60,6 @@ const doRunForStartCoord = (x: number, y: number, direction: string) => {
                 break;
             }
             case "|": {
-                visited.add(cellToKey(cell.cell, cell.direction))
                 if (cell.direction === "L" || cell.direction === "R") {
                     queue.push({ cell: nextCell, direction: "U" })
                     queue.push({ cell: nextCell, direction: "D" })
@@ -82,8 +78,8 @@ const doRunForStartCoord = (x: number, y: number, direction: string) => {
 
     return [...visited.values()]
         .map(item => {
-            const [x, y] = item.split(".")[0].split(",")
-            return { x: parseInt(x), y: parseInt(y) }
+            const [x, y] = item.split(".")[0].split(",").map(Number)
+            return { x: x, y: y }
         })
         .filter((item, idx, arr) => arr.findIndex(i => i.x === item.x && i.y === item.y) === idx)
         .length
@@ -92,48 +88,19 @@ const doRunForStartCoord = (x: number, y: number, direction: string) => {
 let answer = 0;
 for (let y = 0; y < grid.height; y++) {
     for (let x = 0; x < grid.width; x++) {
-        console.log(answer)
-        if (y === 0 && x === 0) {
-            [doRunForStartCoord(x, y, "R"), doRunForStartCoord(x, y, "D")]
-                .forEach(item => {
-                    if (item > answer) answer = item
-                })
-        }
-        else if (y === 0 && x === grid.width - 1) {
-            [doRunForStartCoord(x, y, "L"), doRunForStartCoord(x, y, "D")]
-                .forEach(item => {
-                    if (item > answer) answer = item
-                })
-        }
-        else if (y === grid.height - 1 && x === 0) {
-            [doRunForStartCoord(x, y, "R"), doRunForStartCoord(x, y, "U")]
-                .forEach(item => {
-                    if (item > answer) answer = item
-                })
-        }
-        else if (y === grid.height - 1 && x === grid.width - 1) {
-            [doRunForStartCoord(x, y, "L"), doRunForStartCoord(x, y, "U")]
-                .forEach(item => {
-                    if (item > answer) answer = item
-                })
-        }
-        else {
-            if (y === 0) {
-                const run = doRunForStartCoord(x, y, "D")
-                if (run > answer) answer = run
-            }
-            if (y === grid.height - 1) {
-                const run = doRunForStartCoord(x, y, "U")
-                if (run > answer) answer = run
-            }
-            if (x === 0) {
-                const run = doRunForStartCoord(x, y, "R")
-                if (run > answer) answer = run
-            }
-            if (x === grid.width - 1) {
-                const run = doRunForStartCoord(x, y, "L")
-                if (run > answer) answer = run
-            }
+
+        const directions = [] as string[]
+
+        if (y === 0) directions.push("D")
+        if (y === grid.height - 1) directions.push("U")
+        if (x === 0) directions.push("R")
+        if (x === grid.width - 1) directions.push("L")
+
+        if (directions.length === 0) continue;
+
+        for (const direction of directions) {
+            const result = doRunForStartCoord(x, y, direction)
+            if (result > answer) answer = result
         }
     }
 }
