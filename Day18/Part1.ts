65e2305d85846803a0a2e866aad86af01b5c8676
Harmen <harmenschouten@outlook.com>
2023-12-18 06:55:05 +0100

    18

--- Day18/Part1.ts
+++ Day18/Part1.ts
@@ -1,3 +1,66 @@
 import "../utils/index.ts"
 
+type Point = {
+    x: number,
+    y: number
+}
+
 const text = await Deno.readTextFile("./Day18/input.txt")
+
+const input = text.splitLb().map(line => {
+    const [direction,amount, _] = line.split(" ")
+    return {
+        amount: Number(amount),
+        direction,
+    }
+})
+
+const start = { x: 0, y: 0 }
+const items = [start] as Point[]
+const last = () => items[items.length - 1]
+
+input.forEach(line => {
+    switch (line.direction) {
+        case "R": {
+            for (let i = 0; i < line.amount; i++) {
+                items.push({ x: last().x + 1, y: last().y })
+            }
+            break;
+        }
+        case "L": {
+            for (let i = 0; i < line.amount; i++) {
+                items.push({ x: last().x - 1, y: last().y })
+            }
+            break;
+        }
+        case "U": {
+            for (let i = 0; i < line.amount; i++) {
+                items.push({ x: last().x, y: last().y + 1 })
+            }
+            break;
+        }
+        case "D": {
+            for (let i = 0; i < line.amount; i++) {
+                items.push({ x: last().x, y: last().y - 1 })
+            }
+            break;
+        }
+    }
+})
+
+const GetAreaForLoop = (array: Point[]) => {
+    const copy = [...array.slice(1), array[0]]
+    let count = 0
+    for (let i = 0; i < array.length; i++) {
+        const x0 = array[i].x
+        const y0 = array[i].y
+        const x1 = copy[i].x
+        const y1 = copy[i].y
+
+        count += ((x0 * y1) - (x1 * y0))
+    }
+    return Math.abs(count)
+}
+const area = GetAreaForLoop(items) - items.length 
+const answer = (items.length) + Math.floor((area / 2) + 1)
+console.log(answer) 
--- Day18/Part2.ts
+++ Day18/Part2.ts
@@ -1,3 +1,65 @@
 import "../utils/index.ts"
 
+type Point = {
+    x: number,
+    y: number
+}
+
+const directionMap = { "0": "R", "1": "D", "2": "L", "3": "U" } as Record<string, string>
+
 const text = await Deno.readTextFile("./Day18/input.txt")
+
+const input = text.splitLb().map(line => {
+    const [_,__, rest] = line.split(" ")
+    const number = rest.slice(2, 7)
+    const direction = rest.slice(7,8)
+    return {
+        amount: parseInt(number, 16),
+        direction: directionMap[direction],
+    }
+})
+
+const start = { x: 0, y: 0 }
+const items = [] as Point[]
+const currentPositition = {...start}
+let total = 0;
+input.forEach(line => {
+    total += line.amount
+    switch (line.direction) {
+        case "R": {
+            currentPositition.x += line.amount
+            break;
+        }
+        case "L": {
+            currentPositition.x -= line.amount
+            break;
+        }
+        case "U": {
+            currentPositition.y += line.amount
+            break;
+        }
+        case "D": {
+            currentPositition.y -= line.amount
+            break;
+        }
+    }
+    items.push({...currentPositition})
+})
+
+const GetAreaForLoop = (array: Point[]) => {
+    const copy = [...array.slice(1), array[0]]
+    let count = 0
+    for (let i = 0; i < array.length; i++) {
+        const x0 = array[i].x
+        const y0 = array[i].y
+        const x1 = copy[i].x
+        const y1 = copy[i].y
+
+        count += ((x0 * y1) - (x1 * y0))
+    }
+    return Math.abs(count)
+}
+
+const answer = Math.floor((GetAreaForLoop(items) - total) / 2) + 1 + total
+
+console.log(answer)
